e6a8cb6aed10e0f21a68ed2f1d0a4da76e6288b9
ldp6242 <ldp535@mail.missouri.edu>
2014-11-20 20:39:23 -0600

    Files

---
+++ Intro.js
@@ -0,0 +1,116 @@
+"cake".length
+
+confirm('This is an example of using JS to create some interaction on a website. Click OK to continue!');
+
+confirm("Please click ok to continue");
+
+prompt("Where are you from?");
+
+"What is today's date".length
+
+"I'm coding like a chamnp".length > 10
+
+console.log(2 * 5)
+console.log("Hello")
+
+// Here is an example of using the greater than (>) operator.
+console.log(15 > 4); // 15 > 4 evaluates to true, so true is printed.
+
+// Fill in with >, <, === so that the following print out true:
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
+
+if ( "Lorenzo".length >= 7 ) {
+    console.log("What a lucky number" );
+}
+
+if ( "How do I get home".length < 4 ) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+    // What should we do if the condition is false? Fill in here:
+    console.log("We are lost let's turn around!");
+}
+
+// Remember, the order and punctuation matter.
+// If you get an error, check carefully, line by line.
+// If you're really stuck, click "Stuck? Get a hint!"
+if ("My name is".length < 5)
+{
+    console.log("The condition is true");   
+}
+else
+{
+    console.log("The condition is false");   
+}
+
+if (10 === 10) {
+    console.log("You got a true!");
+} else {
+    console.log("You got a false!");
+}
+
+if (12 / 4 === "Ari".length) {
+    confirm("Will this run the first block?");
+} else {
+    confirm("Or the second block?");
+}
+
+if("Jon".length * 2 / (2+1) === 2 )
+{
+    console.log("The answer makes sense!");
+} 
+else { 
+    console.log("Error Error Error");
+}
+
+//Modulo
+console.log(14 % 3);
+console.log(99 % 8);
+console.log(11 % 3);
+
+if( 20 % 10 === 0 ) {
+    console.log("The first number is even");
+} else {
+    console.log("The first number is odd");
+}
+
+"wonderful day".substring(3, 7)
+
+// Here is an example of the 1st to 4th letter of "JavaScript":
+// console.log("JavaScript".substring(0,4));
+console.log("January".substring(0, 3));
+console.log("Melbourne is great".substring(0, 12));
+console.log("Hamburgers".substring(3, 10));
+
+var myAge = 22;
+console.log(myAge);
+
+// myCountry and give it a string value.
+var myCountry = "United States"
+
+// Use console.log to print out the length of the variable myCountry.
+console.log(myCountry.length );
+
+// Use console.log to print out the first three letters of myCountry.
+console.log(myCountry.substring(0,3) );
+
+// declare a variable myName and give it your name.
+var myName = "Lorenzo"
+// use console.log to print out the myName variable.
+console.log(myName);
+// change the value of myName to be just the first 2 
+// letters of your name.
+myName = myName.substring(0, 2);
+// use console.log to print out the myName variable.
+console.log(myName);
+
+if (10 / 10 === 3 % 2) {
+    console.log("I finished my first course!");
+}
+else {
+    console.log("I still need to review my first course!");
+}
